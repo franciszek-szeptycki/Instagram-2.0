2cd09e300cf0cdf5accdff46179cd9fef64c8bd2
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-11-06 21:49:12 +0100

    login 1.0

--- client/src/panel/LoginPanel.tsx
+++ client/src/panel/LoginPanel.tsx
@@ -1,27 +1,63 @@
-import * as React from "react";
+import React, {useState} from "react";
+import dataCorrectStatus from "./dataCorrectStatus";
 import Label from "./Label";
 
 interface loginDataInterface {
 	email: string,
-	username: string,
+	password: string,
 }
 
 const labels: string[] = ["email", "username"];
 
 const LoginPanel = () => {
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+
+    const handleInput = ({ name, value }): void => {
+        switch (name) {
+            case "email":
+                return setEmail(value);
+            case "password":
+                return setPassword(value);
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const preparedData: loginDataInterface = {
+			email,
+			password,
+		};
+		
+		dataCorrectStatus(preparedData)
+
+        console.log(preparedData);
+    };
 
     return (
         <div className="login-background">
-            {/* <div className="login panel">
+            <div className="login panel">
                 <p className="panel__title">Title</p>
-                <form className="form panel__form">
+                <form
+                    className="form panel__form"
+                    onSubmit={(e) => handleSubmit(e)}
+                >
                     {labels.map((item: string, index: number) => (
-                        <Label key={index} data={item} />
+                        <Label
+                            key={index}
+                            data={item}
+                            handleInput={handleInput}
+                        />
                     ))}
+                    <label className="form__label">
+                        <input className="form__label-submit" type="submit" />
+                    </label>
                 </form>
-            </div> */}
+            </div>
         </div>
     );
+
 };
 
 export default LoginPanel;
