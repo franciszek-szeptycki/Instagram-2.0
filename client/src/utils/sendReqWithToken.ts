ad59908cec3c77294ab523014d16c43e321e6412
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-11-13 00:22:08 +0100

    commit

--- client/src/utils/sendReqWithToken.ts
+++ client/src/utils/sendReqWithToken.ts
@@ -1,12 +1,13 @@
 // import { getToken } from "./jwt";
 
 export const postCreatedPost = (data) => {
+    console.log(data)
 	fetch("/api/post/add", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: data,
     })
         .then((res) => res.json())
         .then((data) => {
