9aaa53bdf129f53e4c5f2395dc5ebf6ecf17b87f
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-11-15 04:19:03 +0100

    refactored reqServer

--- client/src/utils/reqServer.ts
+++ client/src/utils/reqServer.ts
@@ -1,62 +1,47 @@
 import getToken from "./getToken";
 
-const reqServer = async (method: string, data: object, path: string, tokenRequired: boolean) => {
-    console.log(data)
+const reqServer = async (
+    method: string,
+    data: object,
+    path: string,
+    tokenRequired: boolean
+) => {
+    // console.log(data);
 
-    let headers: HeadersInit
+    let headers: HeadersInit;
     if (tokenRequired) {
         headers = {
             "Content-Type": "application/json",
             Authorization: `Bearer ${getToken()}`,
-        }
+        };
     } else {
-        headers = { "Content-Type": "application/json", }
+        headers = { "Content-Type": "application/json" };
     }
 
+    let fetchContent: object;
     if (method === "POST") {
-        return await fetch(path, {
+        fetchContent = {
             method,
             headers,
             body: JSON.stringify(data),
-            })
-            .then(async (res) => {
-                const {status} = res
-                const {access_token, msg, data} = await res.json();
-    
-                if (access_token) {
-                    localStorage.setItem("access_token", access_token);
-                }
-    
-                console.log(data)
-                return {
-                    status,
-                    msg,
-                    data,
-                };
-            })
-            .catch((error) => {
-                console.error(error);
-                return {
-                    status: 500,
-                    msg: "giant error...",
-                    data: "",
-                }
-            });
-    
-    } else if(method === "GET") {
-    return await fetch(path, {
-        method,
-        headers,
-    })
+        };
+    } else if (method === "GET") {
+        fetchContent = {
+            method,
+            headers,
+        };
+    }
+
+    return await fetch(path, fetchContent)
         .then(async (res) => {
-            const { status } = res
+            const { status } = res;
             const { access_token, msg, data } = await res.json();
 
             if (access_token) {
                 localStorage.setItem("access_token", access_token);
             }
 
-            console.log(data)
+            // console.log(data);
             return {
                 status,
                 msg,
@@ -69,12 +54,10 @@ const reqServer = async (method: string, data: object, path: string, tokenRequir
                 status: 500,
                 msg: "giant error...",
                 data: "",
-            }
+            };
         });
-}
+};
 
-}
-
-export type reqType = Awaited<ReturnType<typeof reqServer>>
+export type reqType = Awaited<ReturnType<typeof reqServer>>;
 
 export default reqServer;
