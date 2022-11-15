fc17d6905170615c11c06c5077d66b4bf6900188
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-11-15 14:44:47 +0100

    nav sass

--- client/src/Container.tsx
+++ client/src/Container.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Nav from './layout/Nav';
-import CreatePostPanel from './widgets/create-post/CreatePostPanel'
+import React from "react";
+import { useSelector } from "react-redux";
+import Nav from "./layout/Nav";
+import CreatePostPanel from "./widgets/create-post/CreatePostPanel";
 import allReducers from "./redux/reducers";
-import Home from './pages/Home';
-    
-type RootState = ReturnType<typeof allReducers>
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+
+type RootState = ReturnType<typeof allReducers>;
 
 const Container = () => {
+    const createPostPanel = useSelector<RootState>(
+        (state) => state.createPostPanel
+    );
 
-	const createPostPanel = useSelector<RootState>(state => state.createPostPanel)
+    return (
+        <div className="container">
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+            {createPostPanel && <CreatePostPanel />}
+        </div>
+    );
+};
 
-	return (<div className="container">
-		<Nav />
-		<Home />
-		{createPostPanel && <CreatePostPanel/>}
-	</div> );
-}
- 
 export default Container;
