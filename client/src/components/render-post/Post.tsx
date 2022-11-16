e2e55a22d4fa9b0f1847d3108962446ee5cc3151
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-11-16 02:08:24 +0100

    rendering posts 1.1

---
+++ client/src/components/render-post/Post.tsx
@@ -0,0 +1,45 @@
+import ProfileIdentity from "../profile-identifier/ProfileIdentity"
+
+
+const Post = ({data}) => {
+	console.log(data)
+
+	if (!data.date) return <></>
+
+	return (
+		<div className="post">
+			<div className="post__header">
+				<ProfileIdentity />
+			</div>
+			<div className="post__main">
+				<div className="post__main-img"></div>
+			</div>
+			<div className="post__footer">
+				<div className="post__footer-top">
+					<div className="post__footer-top-interactions">
+						<button className="post__footer-top-interactions-btn">
+							<i className="fa-regular fa-comment"></i>
+						</button>
+						<button className="post__footer-top-interactions-btn">
+							<i className="fa-regular fa-heart"></i>
+						</button>
+						<button className="post__footer-top-interactions-btn">
+							<i className="fa-solid fa-eye"></i>
+						</button>
+						<button className="post__footer-top-interactions-btn">
+							<i className="fa-regular fa-envelope"></i>
+						</button>
+					</div>
+					<p className="post__footer-top-date">
+						{data.date.split(" ")[1]} {data.date.split(" ")[2]}{" "}
+						{data.date.split(" ")[3]}
+					</p>
+				</div>
+				<p className="post__footer-description">
+					{data.description}
+				</p>
+			</div>
+		</div>)
+}
+
+export default Post;
