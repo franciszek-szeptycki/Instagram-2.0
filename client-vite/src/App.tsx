134c7eba74b21127b42667f91f078e6c9de8cce3
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-12-28 22:19:25 +0100

    src

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -1,12 +1,15 @@
 import {useContext} from "react";
 import {AppContext} from "./context";
+import ProtRoutes from "./prot-routes/ProtRoutes";
+import NonProtRoutes from "./non-prot-routes/NonProtRoutes";
 
 const App = () => {
-    const {state, dispatch} = useContext(AppContext)
+    const {state} = useContext(AppContext)
 
 return (
     <div className="App">
-
+        {state.USER_LOGGED ? (
+            <div id="a"></div>) : (<div id="b"></div>)}
     </div>
 )}
 
--- client-vite/src/context/index.ts
+++ client-vite/src/context/index.ts
@@ -2,8 +2,9 @@ import React, {createContext} from "react";
 import {Action, State} from "./reducers";
 
 export const initialState: State = {
-    USER_LOGGED: false
-}
+    USER_LOGGED: false,
+};
+
 
 export const AppContext = createContext<{state: State, dispatch: React.Dispatch<Action>}>({
     state: initialState,
